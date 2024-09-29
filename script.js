0a5d5cdb7d509e97aa18d356111c0db37dcf3c11
fallenICE6 <Matvey223225@mail.ru>
2024-09-29 20:23:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const textTag= document.getElementById('text_tag')
+const textTag = document.getElementById('text_tag')
 const textInput = document.getElementById('text_input')
 const newDiv = document.createElement('div')
 
 
 function InsertBeforeTag() {
-	if (textInput.value === '') return	
-	newDiv.textContent = textInput.value
-	textTag.parentNode.insertBefore(newDiv, textTag)
+    if (textInput.value === '') return
+    newDiv.textContent = textInput.value
+    textTag.parentNode.insertBefore(newDiv, textTag)
 }
 function InsertAfterTag() {
-	if (textInput.value === '') return
-	newDiv.textContent = textInput.value
-	textTag.parentNode.insertBefore(newDiv, textTag.nextSibling)
+    if (textInput.value === '') return
+    newDiv.textContent = textInput.value
+    textTag.parentNode.insertBefore(newDiv, textTag.nextSibling)
 }
 function ReplaceTag() {
-	if (textInput.value === '') return
-	textTag.textContent = textInput.value
+    if (textInput.value === '') return
+    textTag.textContent = textInput.value
 }
 
 
